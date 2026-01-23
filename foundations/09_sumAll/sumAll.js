8daeee3aceb39c4750209a57aeed668d8d4d4ffd
qile <98936712+csdropout@users.noreply.github.com>
2026-01-23 16:01:28 +0800

    Complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (b < a) {
+    [a, b] = [b, a];
+  }
+  let sum = 0;
+  for (; a <= b; a++) {
+    sum += a;
+  }
+  return sum;
 };
 
 // Do not edit below this line
