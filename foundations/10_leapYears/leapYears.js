b2b8491404a4ed9f5bbbaedd8f380fcf8e404fbd
qile <98936712+csdropout@users.noreply.github.com>
2026-01-23 16:15:21 +0800

    Complete leapYears exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // year divisible by 100 not leap year unless divisble by 400
+  // is leap year if divisible by 4
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else return false;
 };
 
 // Do not edit below this line
