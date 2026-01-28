07a7298463bf18f32b28eb9e23c9a5b1a1714168
qile <98936712+csdropout@users.noreply.github.com>
2026-01-28 18:54:58 +0800

    Complete findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // get ages
+  // sort by age
+  // return oldest (last element)
 
+  const peopleWithAge = people.reduce((arr, person) => {
+    person.yearOfDeath = person.yearOfDeath
+      ? person.yearOfDeath
+      : new Date().getFullYear();
+    arr.push({
+      name: person.name,
+      yearOfBirth: person.yearOfBirth,
+      yearOfDeath: person.yearOfDeath,
+      age: person.yearOfDeath - person.yearOfBirth,
+    });
+    return arr;
+  }, []);
+  console.log(peopleWithAge);
+
+  const sortedByAge = peopleWithAge.sort((a, b) => a.age - b.age)
+  console.log(sortedByAge)
+
+  return sortedByAge[sortedByAge.length - 1]
 };
 
 // Do not edit below this line
