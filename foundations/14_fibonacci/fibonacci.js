d855dd69a2f7ce0c92a53d4d4646a044f505e46a
qile <98936712+csdropout@users.noreply.github.com>
2026-01-28 18:24:07 +0800

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS'
+    
+    dp = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
 };
 
 // Do not edit below this line
