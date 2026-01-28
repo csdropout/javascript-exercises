f0071efb51c2d8c1a58ce460324a0e7cd5d6c188
qile <98936712+csdropout@users.noreply.github.com>
2026-01-28 17:59:50 +0800

    Complete calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, current) => (sum += current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce(
+    (sum, current) => (sum *= current),
+    arr.length >= 1 ? 1 : 0,
+  );
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) {
+    return 1
+  }
+  return number * factorial(number - 1)
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
