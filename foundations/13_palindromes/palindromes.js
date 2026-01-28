f5db6869bc8d80e591d9c5ff38e028a4c76f19f8
qile <98936712+csdropout@users.noreply.github.com>
2026-01-28 18:12:37 +0800

    Complete palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let processedString = string.replaceAll(/[^\w+]/g, '').toLowerCase()
+    for (let i = 0; i < Math.floor(processedString.length / 2); i++) {
+        if (processedString.at(i) !== processedString.at(-1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
